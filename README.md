7c788dd79b076a252fb1bb1d0011f37cf56fb776
Spencer197 <spncr@protonmail.com>
2018-01-07 14:14:43 +0000

    Initial commit 2018.01.07

---
+++ README.md
@@ -0,0 +1,5 @@
+This is the first app of the Complete Ruby on Rails Developer course.
+
+I have had to change the name of this app from 'alpha_blog' to 'alpha-blog'.
+
+This app was made production rready and deployed to Heroku on 2018.01.07
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+root 'pages#home'# This make the homepage the root route, which doesn't need /home on the url to go there.
+#get 'pages/home', to: 'pages#home' - Replaced by the above line.
+get 'about', to: 'pages#about'
+end
