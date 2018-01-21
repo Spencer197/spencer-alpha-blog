4ca5c86cf7aa59d71b131c277d9a78cbda7120e6
Spencer197 <spncr@protonmail.com>
2018-01-21 09:29:30 +0000

    Add users show & gravatar image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def gravatar_for(user, options = { size: 80 })
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"#?s=#{size} enables size to be specified in users/show.html.erb. 
+    image_tag(gravatar_url, alt: user.username, class: "img-circle")#"img-circle" is a bootstrap class that creates a circular image.
+  end
 end
