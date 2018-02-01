4e0e07fc07a9579720d94caeb91e58140aaa9999
Spencer197 <spncr@protonmail.com>
2018-02-01 13:53:57 +0000

    Add article/category association

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
+  has_many :article_categories
+  has_many :articles, through: :article_categories
   validates :name, presence: true, length: { minimum: 3, maximum: 25 }#Specifies minimum & maxium category name length.
   validates_uniqueness_of :name#Ensures that :name is unique.
 end
